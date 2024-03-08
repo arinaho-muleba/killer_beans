6562f4e4ff2f556304df231d8c1d4307a617b0cc
Vutlhari Mashimbyi <global-vutlhari.mashimbyi@bbd,ac.za>
2024-03-08 15:37:27 +0200

    Added views and procedures

---
+++ database/scripts/Views/dbo.orderItemsView.sql
@@ -0,0 +1,14 @@
+CREATE OR ALTER VIEW OrderItemsView AS
+SELECT
+    ol.order_id,
+	ol.quantity,
+    b.name AS bean_name,
+    COUNT(ol.id) AS item_count
+FROM
+    OrderLines ol
+INNER JOIN
+    Beans b ON ol.bean_id = b.id
+GROUP BY
+    ol.order_id,
+    b.name,
+	ol.quantity;
