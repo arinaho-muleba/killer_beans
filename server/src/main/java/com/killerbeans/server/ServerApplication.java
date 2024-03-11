137caa11aceb9e24445118b20c4ba4f0b3907e0b
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-03-11 18:26:50 +0200

    added some greeting

--- server/src/main/java/com/killerbeans/server/ServerApplication.java
+++ server/src/main/java/com/killerbeans/server/ServerApplication.java
@@ -2,12 +2,21 @@ package com.killerbeans.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
+	@GetMapping
+	public String greet(){
+		return  "Welcome to killer beans";
+	}
+
 
 }
